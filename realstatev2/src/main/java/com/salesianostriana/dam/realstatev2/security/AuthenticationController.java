9c57a0954c5ce853518d2ec10451aa9a6b1ad156
Juan Carlos Ardana Murillo <cardamu02@gmail.com>
2021-11-22 18:52:39 +0100

    Progreso en la parte de autentificacion en el login

---
+++ realstatev2/src/main/java/com/salesianostriana/dam/realstatev2/security/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.salesianostriana.dam.realstatev2.security;
+
+import com.salesianostriana.dam.realstatev2.security.dto.LoginDto;
+import com.salesianostriana.dam.realstatev2.users.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
+
+    @PostMapping("/auth/register/user")
+    public ResponseEntity<User> login(@RequestBody LoginDto loginDto){
+        Authentication authentication= authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getEmail(),
+                        loginDto.getPassword()
+                )
+        );
+
+
+    }
+}
