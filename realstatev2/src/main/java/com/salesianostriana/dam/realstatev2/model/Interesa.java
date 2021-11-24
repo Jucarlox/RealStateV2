8466b667ac2e22bf4fddd1810ec5e3308e8f5bf6
Juan Carlos Ardana Murillo <cardamu02@gmail.com>
2021-11-24 01:26:30 +0100

    En pruebas, queda comprobar que borre los intereses de los interesados

---
+++ realstatev2/src/main/java/com/salesianostriana/dam/realstatev2/model/Interesa.java
@@ -0,0 +1,41 @@
+package com.salesianostriana.dam.realstatev2.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.salesianostriana.dam.realstatev2.users.model.User;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Interesa implements Serializable {
+    @Builder.Default
+    @EmbeddedId
+    private InteresaPK id = new InteresaPK();
+
+    @JsonIgnore
+    @ManyToOne
+    @MapsId("vivienda_id")
+    @JoinColumn(name="vivienda_id", foreignKey = @ForeignKey(name = "FK_INTERESA_VIVIENDA"))
+    private Vivienda vivienda;
+
+    @JsonIgnore
+    @ManyToOne
+    @MapsId("interesado_id")
+    @JoinColumn(name="interesado_id", foreignKey = @ForeignKey(name = "FK_INTERESA_INTERESADO"))
+    private User interesado;
+
+
+    @CreatedDate
+    private LocalDateTime createdDate = LocalDateTime.now();
+    private String mensaje;
+}
--- realstatev2/src/main/java/com/salesianostriana/dam/realstatev2/security/SecurityConfig.java
+++ realstatev2/src/main/java/com/salesianostriana/dam/realstatev2/security/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"/vivienda/").authenticated()
                 .antMatchers(HttpMethod.GET,"/vivienda/{id}").authenticated()
                 .antMatchers(HttpMethod.PUT,"/vivienda/{id}").hasAnyRole("ADMIN", "PROPIETARIO")
+                .antMatchers(HttpMethod.DELETE,"/vivienda/{id}").hasAnyRole("ADMIN", "PROPIETARIO")
 
                 .anyRequest().authenticated();
 
