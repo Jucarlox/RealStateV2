de3b53706ef0979c42c7085818d487f4e258c38c
Juan Carlos Ardana Murillo <cardamu02@gmail.com>
2021-11-25 19:19:36 +0100

    Terminada pero recursividad infinita en postman

--- realstatev2/src/main/java/com/salesianostriana/dam/realstatev2/services/InteresaService.java
+++ realstatev2/src/main/java/com/salesianostriana/dam/realstatev2/services/InteresaService.java
@@ -5,6 +5,14 @@ import com.salesianostriana.dam.realstatev2.repository.InteresaRepository;
 import com.salesianostriana.dam.realstatev2.services.base.BaseService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InteresaService extends BaseService<Interesa,Long, InteresaRepository> {
+
+    public List<Interesa> findInteresados (){
+
+        return repositorio.findInteresados();
+
+    }
 }
